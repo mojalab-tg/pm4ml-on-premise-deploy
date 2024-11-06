d161ece3455b349a20fcfa1b2bec07dbc506e6e3
Hamid Barry <hamidou.atd@gmail.com>
2024-11-06 16:37:52 +0000

    Payment manager on premise V1

--- docker-compose/vault/docker-entrypoint.sh
+++ docker-compose/vault/docker-entrypoint.sh
@@ -121,7 +121,7 @@ vault secrets enable -path=secrets kv
 vault secrets tune -max-lease-ttl=97600h pki
 vault write -field=certificate pki/root/generate/internal \
         common_name="example.com" \
-        ttl=97600h
+        ttl=87600h
 vault write pki/config/urls \
     issuing_certificates="http://127.0.0.1:8233/v1/pki/ca" \
     crl_distribution_points="http://127.0.0.1:8233/v1/pki/crl"
